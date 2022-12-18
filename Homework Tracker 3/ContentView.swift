ab1dc92a9868cc60cce2d4619ee440e2890f489d
oliver halvey <oliverhalvey310@yahoo.com>
2022-12-18 00:42:22 -0800

    Initial Commit

---
+++ Homework Tracker 3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Homework Tracker 3
+//
+//  Created by Oliver Halvey on 12/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Homework Tracker 3/Homework_Tracker_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_Tracker_3App.swift
+//  Homework Tracker 3
+//
+//  Created by Oliver Halvey on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_Tracker_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
